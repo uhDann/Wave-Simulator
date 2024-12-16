ba39a2cc10ad544a71dfb41aaea6040ec9f1305b
Danila Kozlov <66920265+uhDann@users.noreply.github.com>
2024-12-16 18:50:51 +0000

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
- # # Step manually and plot
-    # for i in tqdm(range(300)):
-    #     sim.step()
-    #     if i % 30 == 0:  # 5 seconds interval (5 / 0.01 = 500 steps)
-    #         subplot_index = i // 50
-    #         if subplot_index < num_subplots:
-    #             sim.plot(ax=axes[subplot_index])
